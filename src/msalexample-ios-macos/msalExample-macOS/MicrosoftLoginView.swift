92dffb1b7e591b205010b96106d27e32438116b0
Fernando Antivero <v-feanti@microsoft.com>
2022-03-07 13:30:39 -0300

    add macos

--- src/msalexample-ios-macos/msalExample-macOS/MicrosoftLoginView.swift
+++ src/msalexample-ios-macos/msalExample-macOS/MicrosoftLoginView.swift
@@ -2,8 +2,36 @@ import SwiftUI
 import MSAL
 
 struct MicrosoftLoginView: View {
+    @State private var isAuthenticated = false
+
     var body: some View {
-        SharedView("")
+        VStack {
+            Text("Welcome to the SwiftUI MSAL tutorial for macOS")
+            Button {
+                if (!isAuthenticated) {
+                    MSALAuthentication.Signin(MSALWebviewParameters(), completion: { (account, securityToken, error) in
+                        if let _ = error {
+                            return
+                        }
+
+                        isAuthenticated.toggle()
+                    })
+                }
+                else {
+                    // IMPORTANT: this require keychain capabilities to be added and signin with a development team. For more information, please refer
+                    // to https://github.com/AzureAD/microsoft-authentication-library-for-objc/tree/3bc25ad3c38c0f0044e3fc624a841ac4789478c0#macos-only-steps
+                    MSALAuthentication.Signout(MSALWebviewParameters(), completion: { (error) in
+                        if let _ = error {
+                            return
+                        }
+
+                        isAuthenticated.toggle()
+                    })
+                }
+            } label: {
+                isAuthenticated ? Text("Logout") : Text("Login")
+            }
+        }.frame(width: 800, height: 600)
     }
 }
 
