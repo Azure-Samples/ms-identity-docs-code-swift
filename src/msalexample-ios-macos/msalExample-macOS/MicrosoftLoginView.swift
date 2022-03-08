6448f7562b91e0ee8e3e5a710094202c3366ce33
Fernando Antivero <v-feanti@microsoft.com>
2022-03-08 18:37:49 -0300

    add graph call

--- src/msalexample-ios-macos/msalExample-macOS/MicrosoftLoginView.swift
+++ src/msalexample-ios-macos/msalExample-macOS/MicrosoftLoginView.swift
@@ -3,10 +3,12 @@ import MSAL
 
 struct MicrosoftLoginView: View {
     @State private var isAuthenticated = false
+    @State private var graphResult = ""
 
     var body: some View {
         VStack {
             Text("Welcome to the SwiftUI MSAL tutorial for iOS and macOS")
+            Text(graphResult)
             Button {
                 if (!isAuthenticated) {
                     MSALAuthentication.signin(completion: { (account, securityToken, error) in
@@ -15,6 +17,28 @@ struct MicrosoftLoginView: View {
                         }
 
                         isAuthenticated.toggle()
+
+                        guard let meUrl = URL(string: "https://graph.microsoft.com/v1.0/me") else {
+                            return
+                        }
+
+                        var request = URLRequest(url: meUrl)
+                        request.httpMethod = "GET"
+                        request.addValue("Bearer \(securityToken!)", forHTTPHeaderField: "Authorization")
+
+                        URLSession.shared.dataTask(with: request, completionHandler: { data, response, error in
+                            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200, error == nil else {
+                                print(error!.localizedDescription)
+                                return
+                            }
+
+                            if let json = try? JSONSerialization.jsonObject(with: data!, options: .mutableContainers),
+                               let jsonData = try? JSONSerialization.data(withJSONObject: json, options: .prettyPrinted) {
+                                graphResult = String(decoding: jsonData, as: UTF8.self)
+                            } else {
+                                print("An error has ocurred")
+                            }
+                        }).resume()
                     })
                 }
                 else {
@@ -26,11 +50,15 @@ struct MicrosoftLoginView: View {
                         }
 
                         isAuthenticated.toggle()
+                        graphResult = ""
                     })
                 }
             } label: {
                 isAuthenticated ? Text("Sign Out") : Text("Sign In")
             }
         }
+        #if os(macOS)
+        .frame(width: 800, height: 600)
+        #endif
     }
 }
